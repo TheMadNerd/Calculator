06cdc28db9f3e255ef626733ba1b016b404ee0af
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-02-15 19:28:58 +0100

    buttons-and-input

---
+++ Calculator/calculator/src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+
+function App() {
+  return(
+      <div className='calcButtons'>
+
+          <input value='wynik'></input>
+
+          <button>%</button>
+          <button>CE</button>
+          <button>C</button>
+          <button>DELETE</button>
+       
+          <button>1/x</button>
+          <button>x^2</button>
+          <button>V2</button>
+          <button>/</button>
+       
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>*</button>
+        
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+         
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+
+          <button>+/-</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>         
+      </div>   
+  )
+
+
+}
+
+
+export default App;
